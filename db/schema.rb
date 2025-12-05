7195fabdc7d3d9198b8c034c6476f5eebcf7d22d
ngt-n <202113723+ngt-n@users.noreply.github.com>
2025-12-05 22:45:06 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_04_125145) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_05_134345) do
   create_table "posts", force: :cascade do |t|
     t.string "location"
     t.string "text"
