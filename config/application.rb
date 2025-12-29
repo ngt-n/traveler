7d4092f5d768205adc7c487a20123e47317904be
ngt-n <202113723+ngt-n@users.noreply.github.com>
2025-12-29 14:02:20 +0900

    画像投稿機能の追加

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Traveler
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_storage.variant_processor = :mini_magick
+    
   end
 end
