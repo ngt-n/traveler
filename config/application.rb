a0da82327beebaeb10ddcf8c754d4c0993982824
ngt-n <202113723+ngt-n@users.noreply.github.com>
2026-01-04 16:23:39 +0900

    画像表示エラーの修正

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Traveler
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.active_storage.variant_processor = :mini_magick
+    config.active_storage.variant_processor = :vips
     
   end
 end
