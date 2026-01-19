b803991cbfa4db3132261166bf5362733bc0d083
ngt-n <202113723+ngt-n@users.noreply.github.com>
2026-01-19 23:58:44 +0900

    ユーザー機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_18_142313) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_19_142114) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_18_142313) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
