c1c2c39d3f82d1748c1671473231441706141685
ngt-n <202113723+ngt-n@users.noreply.github.com>
2026-01-20 20:49:51 +0900

    ユーザー機能の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_19_142114) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_20_114810) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_19_142114) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
